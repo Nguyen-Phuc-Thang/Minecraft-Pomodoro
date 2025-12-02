19993b8336523ed9fc3eb0d0146346d98524f1f1
conmefo <hoquangminh757@gmail.com>
2025-12-02 19:46:58 +0700

    add slotbar

--- src/scenes/BuildScene.js
+++ src/scenes/BuildScene.js
@@ -11,29 +11,16 @@ export default class BuildScene extends Phaser.Scene {
 
     this.cameras.main.setBackgroundColor(0xaecbff);
 
-    const topBarHeight = 80;
-    const topBar = this.add.rectangle(
-      width / 2,
-      topBarHeight / 2,
-      width,
-      topBarHeight,
-      0x222831
-    );
-    topBar.setOrigin(0.5);
 
-    const bottomBarHeight = 120;
-    const bottomBar = this.add.rectangle(
-      width / 2,
-      height - bottomBarHeight / 2,
-      width,
-      bottomBarHeight,
-      0x393e46
-    );
-    bottomBar.setOrigin(0.5);
+    // slotbar
+    const barLeft = 10; 
+    const barBottom = 10;
+    const barWidth = 5268;
+    const barHeight = 636;
+    const barScale = 0.16;
+    const barX = barLeft + (barWidth * barScale) / 2;
+    const barY = height - barBottom - (barHeight * barScale) / 2;
 
-    this.add.text(20, 20, "Build Screen", {
-      fontSize: "24px",
-      color: "#ffffff",
-    });
+    this.slotBar = this.add.image(barX, barY, "slot_bar").setOrigin(0.5).setScale(barScale);
   }
 }
